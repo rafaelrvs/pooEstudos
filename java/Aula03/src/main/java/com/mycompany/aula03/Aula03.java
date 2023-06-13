a6d3c95de3bf7a0a8a8e24b2657c7d682feb649d
Rafael Vieira <rafaelvieira1333@gmail.com>
2023-06-13 14:52:26 -0300

    update
    new file in aula03

---
+++ java/Aula03/src/main/java/com/mycompany/aula03/Aula03.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aula03;
+
+/**
+ *
+ * @author rafae
+ */
+public class Aula03 {
+
+    public static void main(String[] args) {
+        //criação do pobjeto
+       Caneta c1 = new Caneta();
+       c1.status();
+    }
+}
---
+++ java/Aula03/src/main/java/com/mycompany/aula03/Caneta.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.aula03;
+
+/**
+ *
+ * @author rafae
+ */
+public class Caneta {
+    public String modelo;
+    public String cor;
+    private float ponta;
+    protected int carga;
+    protected  boolean tampada;
+    
+    public void status(){
+        //this = nome do objeto que chamou, referencia ao proprio objeto que chamou
+        
+   
+        System.out.println("Esta tampada? : " + this.tampada);
+        System.out.println("Qual e o modelo ? : " + this.modelo);
+        System.out.println("Ela qual é a ponta ? : " + this.ponta);
+        System.out.println("Ela tem carga ? : " + this.carga);
+
+    }
+    
+   public void escolha(){
+            System.out.println("Você está escrevendo com a caneta "+ this.cor);
+            this.status();
+    }
+    
+    
+    private void rabiscar(){
+        if (tampada == true ) {
+           System.out.println("A caneta " +this.cor +" NÃO pode ficar tampada se quiser rabiscar");
+        }
+        else{
+           System.out.println("Estou rabiscando");
+           escolha();
+            
+        }
+        
+    }
+    protected void tampar(){
+        this.tampada = true;
+        
+    }
+    protected void destampar(){
+        this.tampada = false;
+    }
+    
+}
